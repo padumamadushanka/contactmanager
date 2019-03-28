a0fb432b8441aec7c9344a31f410235e1ada8ce1
paduma <madushankapaduma@gmail.com>
2019-03-28 15:44:10 +0530

    Updates

---
+++ precache-manifest.52568332d1c62b6fd2597174a44ae63e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "17307174cfbe4fac0ce0",
+    "url": "/contactmanager/static/js/main.17307174.chunk.js"
+  },
+  {
+    "revision": "1767e6891f17437bf55c",
+    "url": "/contactmanager/static/js/2.1767e689.chunk.js"
+  },
+  {
+    "revision": "17307174cfbe4fac0ce0",
+    "url": "/contactmanager/static/css/main.8b986a94.chunk.css"
+  },
+  {
+    "revision": "1767e6891f17437bf55c",
+    "url": "/contactmanager/static/css/2.78dba6e8.chunk.css"
+  },
+  {
+    "revision": "4d5cbeb774e55e585b0821ae6bbaa7e5",
+    "url": "/contactmanager/index.html"
+  }
+];
